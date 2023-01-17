ee011d20a32de67c62bba6fb030cd4e2e417cc57
Jiyoon <dpdwldbs0911@gmail.com>
2023-01-17 14:54:41 +0900

    230117

---
+++ Baekjoon/Backtracking/1038. 감소하는 수.py
@@ -0,0 +1,20 @@
+from itertools import combinations, permutations
+
+n = int(input())
+
+def decrease(n):
+    for i in range(10):
+        numbers.append(i)
+    for i in range(2,11):
+        for j in combinations(range(10),i):
+            numbers.append(int(''.join(map(str, ((j)[::-1])))))
+    numbers.sort()
+    # print(len(numbers))
+    if n>=1023:
+        print("-1")
+    else:
+        print(numbers[n])
+
+
+numbers = []
+decrease(n)
