12dfc62f65ab5dae59c7aa24190880b8461b327d
Jiyoon <dpdwldbs0911@gmail.com>
2023-01-21 15:57:57 +0900

    230121

--- Baekjoon/Backtracking/1941. 소문난 칠공주.py
+++ Baekjoon/Backtracking/1941. 소문난 칠공주.py
@@ -10,34 +10,28 @@ def bfs(arr):
     dr = [-1, +1, 0, 0]
     dc = [0, 0, -1, +1]
 
-    # 7명 여학생 위치 방문처리를 위한 리스트 1로 처리
     visited = [[1] * 5 for _ in range(5)]
-
-    # 7명의 여학생 위치를 0으로 초기화
     for i in arr:
         visited[i[0]][i[1]] = 0
-    # 첫번째 여학생의 위치를 큐에 넣는다.
     queue = deque([(arr[0])])
-    # 첫번째 여학생의 방문처리를 1로 변경
     visited[arr[0][0]][arr[0][1]] = 1
     check = 1  # 여학생들의 위치 방문 횟수(첫 위치 방문했기 때문에 1)
     while queue:
-        r, c = queue.popleft()  # 큐에 있는 위치 꺼내기
+        r, c = queue.popleft()
 
-        for i in range(4):  # 델타 위치를 이동하면서
+        for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
-            # 범위를 벗어나면 진행x
             if nr < 0 or nr >= 5 or nc < 0 or nc >= 5:
                 continue
             # 만약 위치를 이동하다 아직 여학생 위치를 방문안했다면
             if not visited[nr][nc]:
-                visited[nr][nc] = 1  # 그위치를 1로 바꿔주고
-                check += 1  # 방문 횟수를 1 증가
-                queue.append((nr, nc))  # 큐에 추가
-    if check != 7:  # 7번 다 방문하지 않았다면
+                visited[nr][nc] = 1 
+                check += 1
+                queue.append((nr, nc))
+    if check != 7:
         return False
-    else:  # 7번 방문했다면
+    else:
         return True
 
 def dfs(depth, start, count):
@@ -55,13 +49,13 @@ def dfs(depth, start, count):
         return
 
     for i in range(start, 25): # 1차원으로 늘려놓음 일단
-        # r = 0, 0, 0, 0, 0, 1,1, 1 ...
+        # r = 0, 0, 0, 0, 0, 1, 1, 1 ...
         # c = 0, 1, 2, 3, 4, 0, 1, 2, 3...
         r = i // 5
         c = i % 5
-        arr.append((r, c))  # 해당 위치를 추가
-        dfs(depth + 1, i + 1, count + (students[r][c] == 'Y'))  # 재귀 돈다.
-        arr.pop()  # 해당 위치를 제거
+        arr.append((r, c))
+        dfs(depth + 1, i + 1, count + (students[r][c] == 'Y'))
+        arr.pop()
 
 
 students = [list(input()) for _ in range(5)]
