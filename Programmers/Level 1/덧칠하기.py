f734d6eed39c32145ef28aa70e633e96c1bd3a43
Jiyoon <dpdwldbs0911@gmail.com>
2023-04-12 01:03:27 +0900

    230412

---
+++ Programmers/Level 1/덧칠하기.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(n, m , section):
+    
+    answer = 0
+    section = deque(section)
+    
+    while section:
+        start = section.popleft()
+        print(start)
+        
+        while section and start + m > section[0]: 
+            section.popleft()
+        answer += 1
+    
+    return answer
