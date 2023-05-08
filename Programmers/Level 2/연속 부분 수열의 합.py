fc1cd681a0950e4b08d05368c615d206d51b5c12
Jiyoon <dpdwldbs0911@gmail.com>
2023-05-09 00:50:54 +0900

    230509

---
+++ Programmers/Level 2/연속 부분 수열의 합.py
@@ -0,0 +1,7 @@
+def solution(elements):
+    cycle = elements + elements
+    s = []
+    for i in range(len(elements)):
+        for j in range(len(elements)):
+            s.append(sum(cycle[i:i+j]))
+    return len(set(s))
