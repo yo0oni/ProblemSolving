7b577d8187c420033c81737c1791d1a889c8e558
Jiyoon <dpdwldbs0911@gmail.com>
2023-06-25 23:47:30 +0900

    230625

--- Programmers/Level 2/압축.py
+++ Programmers/Level 2/압축.py
@@ -1,26 +1,17 @@
 def solution(msg):
     answer = []
-    eng = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    
-    while len(msg) > 0:
-        w = msg[0]
-        cnt = 0
-        if len(msg) == 1:
-            answer.append(eng.index(w)+1)
+    dic = {chr(i + 64): i for i in range(1, 27)}
+
+    w = c = 0
+    while True:
+        c += 1	
+        if c == len(msg):	
+            answer.append((dic[msg[w:c]]))
             break
-
-        while len(w) < len(msg):
-            s = w
-            cnt += 1
-            w += msg[cnt]
-            if w not in eng:
-                w = s
-                break
-
-        answer.append(eng.index(w)+1)
-        msg = msg[len(w):]
-
-        if msg:
-            eng.append(w + msg[0])
+        
+        if msg[w:c+1] not in dic:
+            dic[msg[w:c+1]] = len(dic) + 1 
+            answer.append(dic[msg[w:c]])
+            w = c
             
     return answer
