28c86c7c20e2f5cedc4021ba9fc6759835098dfb
Jiyoon Kim <rlawldbs0911@naver.com>
2023-11-20 12:47:24 +0900

    231120

---
+++ Programmers/SQL/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,5 @@
+SELECT concat("/home", "/grep", "/src/", f.BOARD_ID, "/", f.FILE_ID, f.FILE_NAME, f.FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_BOARD b, USED_GOODS_FILE f
+WHERE b.BOARD_ID = f.BOARD_ID
+    AND b.views = (SELECT max(views) FROM USED_GOODS_BOARD)
+ORDER BY f.FILE_ID DESC
