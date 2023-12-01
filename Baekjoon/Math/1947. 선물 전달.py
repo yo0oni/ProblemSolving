581eff2ef0b68adf75533eef0e3f1b8696f33e5e
Jiyoon Kim <rlawldbs0911@naver.com>
2023-12-01 23:39:41 +0900

    231201

---
+++ Baekjoon/Math/1947. 선물 전달.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0 for _ in range(N+1)]
+dp[0] = 1;
+for i in range(2, N+1):
+    dp[i] = ((i-1)*dp[i-2] + (i-1)*dp[i-1]) % 1000000000
+
+print(dp[N])
