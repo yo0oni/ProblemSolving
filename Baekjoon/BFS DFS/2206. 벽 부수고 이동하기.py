6ad2919c058f5fb5a75e6cc59ab4b422eb44f7bd
Jiyoon Kim <rlawldbs0911@naver.com>
2024-01-07 21:14:40 +0900

    240107

--- Baekjoon/BFS DFS/2206. 벽 부수고 이동하기.py
+++ Baekjoon/BFS DFS/2206. 벽 부수고 이동하기.py
@@ -3,35 +3,38 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-data = [list(map(int, input())) for _ in range(n)]
+data = []
+for _ in range(n):
+    data.append(list(map(int, input().strip())))
+
 visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
 visited[0][0][0] = 1
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(a, b, c) :
-    q = deque()
-    q.append((a, b, c))
+def bfs(a, b, c):
+    queue = deque()
+    queue.append((a, b, c))
 
-    while q :
-        x, y, z = q.popleft()
-        if x == n - 1 and y == m - 1 :
-            return visited[x][y][z]
+    while queue:
+        x, y, z = queue.popleft()
 
-        for d in range(4) :
-            nx = x + dx[d]
-            ny = y + dy[d]
-            if not (0 <= nx < n and 0 <= ny < m) :
-                continue
+        if x == n - 1 and y == m - 1: # 끝에 도달
+            return visited[x][y][z] # 최종 거리 출력
 
-            if data[nx][ny] == 1 and z == 0 :
-                visited[nx][ny][1] = visited[x][y][0] + 1
-                q.append((nx, ny, 1))
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            elif data[nx][ny] == 0 and visited[nx][ny][z] == 0 :
-                visited[nx][ny][z] = visited[x][y][z] + 1
-                q.append((nx, ny, z))
+            if 0 <= nx < n and 0 <= ny < m:
+                if data[nx][ny] == 1 and z == 0:
+                    visited[nx][ny][1] = visited[x][y][0] + 1 # 거리 갱신
+                    queue.append((nx, ny, 1))
+
+                elif data[nx][ny] == 0 and visited[nx][ny][z] == 0:
+                    visited[nx][ny][z] = visited[x][y][z] + 1 # 거리 갱신
+                    queue.append((nx, ny, z))
 
     return -1
 
