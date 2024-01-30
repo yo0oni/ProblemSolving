04f2cf84f248dc89a3d927a3354aa4d7d79813da
Kim Jiyoon <rlawldbs0911@naver.com>
2024-01-30 10:51:26 +0900

    240130

---
+++ Baekjoon/BFS DFS/1058. 친구.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+graph = [list(input().strip()) for _ in range(n)]
+# 2-친구 사이일 때 1, 아니면 0
+friends = [[0] * n for _ in range(n)]
+
+for k in range(n):
+  for i in range(n):
+    for j in range(n):
+      if i == j:
+        continue
+      # 2-친구인 경우
+      if graph[i][j] == 'Y' or (graph[i][k] == 'Y' and graph[k][j] =='Y'):
+        friends[i][j] = 1
+
+answer = 0
+for row in friends:
+  answer = max(answer, sum(row))
+print(answer)
