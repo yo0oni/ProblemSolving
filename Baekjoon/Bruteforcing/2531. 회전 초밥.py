bd4335167f86a8d16915eaadce5087559e647f63
Kim Jiyoon <rlawldbs0911@naver.com>
2024-02-09 21:59:19 +0900

    240209

---
+++ Baekjoon/Bruteforcing/2531. 회전 초밥.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N, d, k, c = map(int, input().rstrip().split())
+sushi = [int(input()) for _ in range(N)]
+max_number_of_type = 0
+
+for i in range(N):
+    if i+k > N:
+        number_of_type = len(set(sushi[i:N] + sushi[:(i+k)%N] + [c]))
+    else:
+        number_of_type = len(set(sushi[i:i+k] + [c]))
+
+    if max_number_of_type < number_of_type:
+        max_number_of_type = number_of_type
+
+print(max_number_of_type)
