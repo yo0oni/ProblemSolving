6d7511fcde3a287060fa824ce44fa24e3a6d5b9d
Kim Jiyoon <rlawldbs0911@naver.com>
2024-05-04 13:34:37 +0900

    240504

---
+++ Baekjoon/Dynamic Programming/11726. 2xn 타일링.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def solution(n):
+    if n == 1:
+        return 1
+    
+    else:
+        dp = [0] * (n+1)
+
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n+1):
+            dp[i] = (dp[i-2] + dp[i-1]) % 10007
+        
+        return dp[n]
+
+n = int(input())
+print(solution(n))
